9ef3aef359ce72b8983883b4fbb7d766b745e655
dhritix1999 <dhritiadyanthaya14@gmail.com>
2021-05-31 23:55:07 +0400

    Added cart utils

---
+++ AUSBookstore/bookstore/utils.py
@@ -0,0 +1,50 @@
+import json
+from .models import *
+
+
+def cookieCart(request):
+    try:
+        cart = json.loads(request.COOKIES['cart'])
+    except:
+        cart = {}
+        print('CART:', cart)
+
+    items = []
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
+    cartItems = order['get_cart_items']
+
+    for i in cart:
+        # We use try block to prevent items in cart that may have been removed from causing error
+        try:
+            if (cart[i]['quantity'] > 0):  # items with negative quantity = lot of freebies
+                cartItems += cart[i]['quantity']
+
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
+
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
+
+                item = {
+                    'id': product.id,
+                    'product': {'id': product.id, 'name': product.name, 'price': product.price,
+                                'image_url': product.image_url}, 'quantity': cart[i]['quantity'],
+                     'get_total': total,
+                }
+                items.append(item)
+
+        except:
+            pass
+
+    return {'cartItems': cartItems, 'order': order, 'items': items}
+
+
+def cartData(request):
+    if request.user.is_authenticated:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
+    return {'cartItems': cartItems, 'order': order, 'items': items}
+
